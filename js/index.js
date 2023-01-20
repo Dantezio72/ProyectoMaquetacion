6f9a77c37a30f6e2ddc01634c1d3144258435b64
Marisol Rodriguez <wvk_28@hotmail.com>
2023-01-20 00:24:56 -0300

    se agrego javascript

---
+++ js/index.js
@@ -0,0 +1,39 @@
+const isla = document.querySelector('.isla')
+const pantano = document.querySelector('.pantano')
+const montaña = document.querySelector('.montaña')
+const bosque = document.querySelector('.bosque')
+const llanura = document.querySelector('.llanura')
+
+// pantano.style.display = 'none'
+// montaña.style.display = 'none'
+// bosque.style.display = 'none'
+// llanura.style.display = 'none'
+
+// function showHide( escuderia )
+// {
+//     if( escuderia == 'isla' ){
+//         pantano.style.display = 'none';
+//         isla.style.display = 'flex';
+//         montaña.style.display = 'none';
+//         bosque.style.display = 'none';
+//     }
+//     else if( escuderia == 'pantano' ){
+//         isla.style.display = 'none';
+//         pantano.style.display = 'flex';
+//         montaña.style.display = 'none';
+//         bosque.style.display = 'none';
+//     }
+//     else if( escuderia == 'montaña' ){
+//         isla.style.display = 'none';
+//         pantano.style.display = 'none';
+//         montaña.style.display = 'flex';
+//         bosque.style.display = 'none';
+//     }
+//     else if( escuderia == 'bosque' ){
+//         isla.style.display = 'none';
+//         pantano.style.display = 'none';
+//         montaña.style.display = 'none';
+//         bosque.style.display = 'flex';
+//     }
+
+// }
